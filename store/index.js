b7cc83ca0425d64ac729c0652875c5833e97b8ad
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2022-09-23 00:40:37 -0300

    feat: atualizando yarn.lock

--- store/index.js
+++ store/index.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import { applyMiddleware, combineReducers, compose, configureStore } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { config } from './ducks'
@@ -9,6 +9,6 @@ const reducer = combineReducers({
   config
 })
 
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+const store = configureStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
 export default store
